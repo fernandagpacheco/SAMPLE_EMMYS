93751442e44fa37bf6ed9c547e1722cceeb67919
Fernanda G. Pacheco <ferrpacheco@gmail.com>
2025-07-16 17:33:12 -0300

    aula 03 property e metodos de classe

--- emmys.py
+++ emmys.py
@@ -2,19 +2,29 @@ class Nominees:
     nominations = []
 
     def __init__(self, title, category):
-        self.title = title 
-        self.category = category
-        self.active = False
+        self._title = title.upper()
+        self._category = category.title()
+        self._active = False
         Nominees.nominations.append(self)
 
     def __string__(self):
-        return f"{self.title} | {self.category}"
+        return f"{self._title} | {self._category}"
     
-    def list_nominations():
-        for nominees in Nominees.nominations:
-            print(f"{nominees.title} | {nominees.category} | {nominees.active}")
+    @classmethod
+    def list_nominations(cls):
+        print(f"{"Nominee".ljust(25)} {"Category".ljust(25)} {"Status"}")
+        for nominee in cls.nominations:
+            print(f"{nominee._title.ljust(25)}  {nominee._category.ljust(25)}  {nominee.active}")
 
-nominees_andor = Nominees("Andor", "Drama")
-nominees_severance = Nominees ("Severance", "Drama")
+    @property
+    def active(self):
+        return "☑" if self._active else "☐"
+    
+    def change_status(self):
+        self._active = not self._active
+
+nominee_andor = Nominees("Andor", "Drama")
+nominee_andor.change_status()
+nominee_severance = Nominees ("Severance", "Drama")
 
 Nominees.list_nominations()
